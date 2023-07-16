04d25fa48091bb2fe7144ef7da9e2299b2671d14
Dainius Jocas <dainius.jocas@gmail.com>
2023-07-17 01:15:24 +0300

    is able to take from classpath

--- src/journal/vespa/linguistics/vap/src/main/java/lt/jocas/vespa/linguistics/LuceneLinguistics.java
+++ src/journal/vespa/linguistics/vap/src/main/java/lt/jocas/vespa/linguistics/LuceneLinguistics.java
@@ -2,16 +2,44 @@ package lt.jocas.vespa.linguistics;
 
 import com.google.inject.Inject;
 import com.yahoo.language.Linguistics;
-import com.yahoo.language.process.Tokenizer;
+import com.yahoo.language.detect.Detector;
+import com.yahoo.language.process.*;
+import com.yahoo.language.simple.SimpleDetector;
 import com.yahoo.language.simple.SimpleLinguistics;
+import com.yahoo.language.simple.SimpleNormalizer;
+import com.yahoo.language.simple.SimpleTransformer;
 
+/**
+ * Factory of Lucene based linguistics processor.
+ *
+ * TODO: docs for all available analysis components.
+ * TODO: deploy vap and print all components in constructor or somewhere.
+ * TODO: should I provide default settings for languages?
+ */
 public class LuceneLinguistics extends SimpleLinguistics {
 
-    private final LuceneTokenizer tokenizer;
+    // Threadsafe instances
+    private final Normalizer normalizer;
+    private final Transformer transformer;
+    private final Detector detector;
+    private final CharacterClasses characterClasses;
+    private final GramSplitter gramSplitter;
+    private final Tokenizer tokenizer;
 
     @Inject
     public LuceneLinguistics(LuceneAnalysisConfig config) {
-        tokenizer = new LuceneTokenizer(config);
+        this.normalizer = new SimpleNormalizer();
+        this.transformer = new SimpleTransformer();
+        this.detector = new SimpleDetector();
+        this.characterClasses = new CharacterClasses();
+        this.gramSplitter = new GramSplitter(characterClasses);
+        this.tokenizer = new LuceneTokenizer(config);
+    }
+
+
+    @Override
+    public Stemmer getStemmer() {
+        return new StemmerImpl(tokenizer);
     }
 
     @Override
@@ -20,5 +48,25 @@ public class LuceneLinguistics extends SimpleLinguistics {
     }
 
     @Override
-    public boolean equals(Linguistics other) { return (other instanceof LuceneLinguistics); }
+    public Normalizer getNormalizer() { return normalizer; }
+
+    @Override
+    public Transformer getTransformer() { return transformer; }
+
+    @Override
+    public Segmenter getSegmenter() { return new SegmenterImpl(getTokenizer()); }
+
+    @Override
+    public Detector getDetector() { return detector; }
+
+    @Override
+    public GramSplitter getGramSplitter() { return gramSplitter; }
+
+    @Override
+    public CharacterClasses getCharacterClasses() { return characterClasses; }
+
+    @Override
+    public boolean equals(Linguistics other) {
+        // Check also equality of the LuceneAnalysisConfig
+        return (other instanceof LuceneLinguistics); }
 }
--- src/journal/vespa/linguistics/vap/src/main/java/lt/jocas/vespa/linguistics/LuceneTokenizer.java
+++ src/journal/vespa/linguistics/vap/src/main/java/lt/jocas/vespa/linguistics/LuceneTokenizer.java
@@ -7,73 +7,46 @@ import com.yahoo.language.process.Tokenizer;
 import com.yahoo.language.simple.SimpleToken;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LuceneTokenizer implements Tokenizer {
 
-    // Here list of current analyzers per language
-    // the idea is not to create analyzers until they are needed
-    // Analyzers are thread safe so no need to recreate them for every document
-    private final LuceneAnalysisConfig config;
+    private final static String FIELD_NAME = "field";
 
-    private final Map<Language, Analyzer> languageAnalyzers = new HashMap<>();
+    private final AnalyzerFactory analyzerFactory;
 
     public LuceneTokenizer(LuceneAnalysisConfig config) {
-        this.config = config;
-    }
-
-    private Analyzer getAnalyzer(Language language) {
-        if (null == languageAnalyzers.get(language)) {
-            Analyzer analyzer = null;
-            try {
-                analyzer = CustomAnalyzer.builder()
-                        .withTokenizer(
-                                "standard", new HashMap<>(Map.of("maxTokenLength", "4")))
-                        .build();
-            } catch (IOException e) {
-                // TODO: what to Use as an analyzer in case resources are missing?
-                // Definitely log WARNING
-                // Since the resources should be in VAP, unit tests must catch the problem and prevent
-                // VAP being deployed
-                throw new RuntimeException(e);
-            }
-            // Store for future uses
-            languageAnalyzers.put(language, analyzer);
-            return analyzer;
-        } else {
-            return languageAnalyzers.get(language);
-        }
-    }
-
-    private Iterable<Token> textToTokens(String input, Analyzer analyzer) {
-        List<Token> tokens = new ArrayList<>();
-        TokenStream fieldName = analyzer.tokenStream("fieldName", input);
-
-        CharTermAttribute charTermAttribute = fieldName.addAttribute(CharTermAttribute.class);
-        try {
-            fieldName.reset();
-            while(fieldName.incrementToken()) {
-                tokens.add(new SimpleToken(charTermAttribute.toString())
-                        .setTokenString(charTermAttribute.toString()));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return tokens;
+        this.analyzerFactory = new AnalyzerFactory(config);
     }
 
     @Override
     public Iterable<Token> tokenize(String input, Language language, StemMode stemMode, boolean removeAccents) {
         if (input.isEmpty()) return List.of();
 
-        // Should we combine language + stemMode + removeAccents to make more variations available?
-        return textToTokens(input, getAnalyzer(language));
+        return textToTokens(input, analyzerFactory.getAnalyzer(language, stemMode, removeAccents));
+    }
+
+    private Iterable<Token> textToTokens(String input, Analyzer analyzer) {
+        List<Token> tokens = new ArrayList<>();
+        TokenStream tokenStream = analyzer.tokenStream(FIELD_NAME, input);
+
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        try {
+            tokenStream.reset();
+            while(tokenStream.incrementToken()) {
+                // TODO: add other attributes, length, position, etc.
+                // TODO: what to do with cases when multiple tokens are inserted into the position?
+                tokens.add(new SimpleToken(charTermAttribute.toString(), charTermAttribute.toString()));
+            }
+            tokenStream.end();
+            tokenStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to analyze: " + input, e);
+        }
+        return tokens;
     }
 }
